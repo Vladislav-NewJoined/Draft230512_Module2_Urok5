cebc545e40e36d48367320490f957674ea4c6c1e
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-05-16 13:26:19 +0300

    Initial commit

--- src/Draft4_Task2_5_4.java
+++ src/Draft4_Task2_5_4.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Draft4_Task2_5_4 {
 
@@ -14,6 +13,7 @@ public class Draft4_Task2_5_4 {
 
 
         // Считываем переменные double из файла по адресу: \\10_doubles.txt
+        System.out.println("Создаем файл txt. В нем 10 дробных чисел. Файл создан в корневом каталоге проекта по адресу: \\10_doubles.txt");
         System.out.println("В файле содержатся следующие переменные: [4.353, 8.121, 1.233, 5.637, 3.001, 4.286, 2.367, 4.551, 2.992, 5.777]");
         System.out.println("Выводим те переменные, которые больше числа пи: ");
         System.out.println();
